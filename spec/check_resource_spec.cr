f972a58561f883e4d77ce326c95fdc153bb5e100
JT Archie <jtarchie@gmail.com>
2018-09-10 13:34:01 -0600

    crystal format

--- spec/check_resource_spec.cr
+++ spec/check_resource_spec.cr
@@ -8,15 +8,14 @@ describe Station::CheckResource do
       resource = Resource.new(
         name: "1ms",
         type: "time",
-        source: { "interval" => "1ms" }
+        source: {"interval" => "1ms"}
       )
       checker = CheckResource.new(resource)
       checker.perform!
       checker.versions.size.should eq 1
     end
   end
-  
-  context "when given a previous version" do
 
+  context "when given a previous version" do
   end
 end
--- src/check_resource.cr
+++ src/check_resource.cr
@@ -16,14 +16,14 @@ module Station
         args: ["run", "-i", "--rm", "concourse/#{@resource.type}-resource", "/opt/resource/check"],
         output: @stdout,
         error: @stderr,
-        input: IO::Memory.new({source: @resource.source, version: version }.to_json)
+        input: IO::Memory.new({source: @resource.source, version: version}.to_json)
       )
     end
 
     def versions : Array(Hash(String, String))
       if stdout = @stdout
         return JSON.parse(stdout.to_s).as_a.map do |version|
-          version.as_h.to_a.map {|k, v| [k, v.as_s] }.to_h
+          version.as_h.to_a.map { |k, v| [k, v.as_s] }.to_h
         end
       else
         return [] of Hash(String, String)

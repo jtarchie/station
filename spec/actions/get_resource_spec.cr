5894e3db49dc80249f811b6a88379ac8d1d798c6
JT Archie <jtarchie@gmail.com>
2018-09-17 20:41:39 -0600

    WIP get resource

--- spec/actions/get_resource_spec.cr
+++ spec/actions/get_resource_spec.cr
@@ -1,12 +1,38 @@
 require "../spec_helper"
+require "random/secure"
 
 include Station
 
 describe Station::Actions::GetResource do
+  resource = Resource.new(
+    name: "echo",
+    type: "echo",
+    source: {"key" => "value"}
+  )
+  base_dir = File.expand_path(File.join(__DIR__, "..", "..", "tmp"))
+
   it "uses a destination directory" do
+    get = Actions::GetResource.new(
+      resource: resource,
+      destionation_dir: File.join(base_dir, Random::Secure.hex),
+      params: {} of String => String
+    )
+    get.perform!(
+      version: {
+        "ref" => "abcd123"
+      } of String => String
+    )
+    contents = File.read(File.join(get.destionation_dir, "version"))
+    contents.chomp.should eq %q[{ "ref": "abcd123" }].chomp
+  end
+
+  it "uses the params" do
 
   end
-  it "uses the params"
-  it "uses the version provided"
-  it "uses source"
+  it "uses the version provided" do
+
+  end
+  it "uses source" do
+
+  end
 end
--- src/actions/get_resource.cr
+++ src/actions/get_resource.cr
@@ -1,7 +1,43 @@
+require "tmpdir"
+
 module Station
   module Actions
     class GetResource
+      getter destionation_dir : String
 
+      def initialize(
+        @resource : Resource,
+        @params : Hash(String, String),
+        @destionation_dir : String = Dir.mktmpdir,
+        @resource_types : ResourceTypes = ResourceTypes.new,
+        @stdout : IO::Memory = IO::Memory.new,
+        @stderr : IO::Memory = IO::Memory.new
+      )
+      end
+
+      def perform!(
+        version : Hash(String, String) = {} of String => String
+      )
+        Process.run(
+          command: "docker",
+          args: [
+            "run", "-i", "--rm",
+            "-v", "#{@destionation_dir}:/tmp/build/get",
+            "-w", "/tmp/build/get",
+            @resource_types.repository(@resource.type),
+            "/opt/resource/in", "/tmp/build/get"
+          ],
+          output: @stdout,
+          error: @stderr,
+          input: IO::Memory.new({
+            source: @resource.source,
+            version: version,
+            params: @params,
+          }.to_json)
+        )
+        puts @stderr.to_s
+        puts @stdout.to_s
+      end
     end
   end
 end

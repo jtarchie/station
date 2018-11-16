126154cb45ce5e696f1ec14b4b334945abca7646
JT Archie <jarchie@pivotal.io>
2018-11-15 18:14:53 -0700

    some refactoring

--- lib/station/executor/job.rb
+++ lib/station/executor/job.rb
@@ -16,20 +16,11 @@ module Station
           steps.each do |step|
             case step
             when Station::Actions::CheckResource
-              result = step.perform!
-              @versions[step.resource.name] += result.payload
-              results[step.to_s] = [result.status]
+              peform_check(results, step)
             when Station::Actions::GetResource
-              result = step.perform!(
-                version: @versions[step.resource.name].last,
-                destination_dir: @volumes[step.resource.name]
-              )
-              results[step.to_s] = [result.status]
+              peform_get(results, step)
             when Station::Actions::PutResource
-              result = step.perform!(
-                mounts_dir: @volumes[step.resource.name]
-              )
-              results[step.to_s] = [result.status]
+              peform_put(results, step)
             else
               raise "unsupported step to execute: #{step.inspect}"
             end
@@ -38,6 +29,29 @@ module Station
         end
         results
       end
+
+      private
+
+      def peform_put(results, step)
+        result = step.perform!(
+          mounts_dir: @volumes[step.resource.name]
+        )
+        results[step.to_s] = [result.status]
+      end
+
+      def peform_get(results, step)
+        result = step.perform!(
+          version: @versions[step.resource.name].last,
+          destination_dir: @volumes[step.resource.name]
+        )
+        results[step.to_s] = [result.status]
+      end
+
+      def peform_check(results, step)
+        result = step.perform!
+        @versions[step.resource.name] += result.payload
+        results[step.to_s] = [result.status]
+      end
     end
-end
+  end
 end
